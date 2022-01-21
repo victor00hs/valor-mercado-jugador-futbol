c1a23acf2ac71a70037af6ee6a408099aea3e925
Ramon Iñiguez <44062098+riniguez91@users.noreply.github.com>
2022-01-21 15:50:40 +0100

    Added best valued at market value players KPI

--- api/app.py
+++ api/app.py
@@ -4,14 +4,20 @@ import json
 from flask import Flask, request
 from flask_cors import CORS
 
-#Global variables
-spark = SparkSession.builder.appName("main").master("local[*]").getOrCreate() # Initialize spark
-spark._jsc.hadoopConfiguration().set("google.cloud.auth.service.account.json.keyfile","data/gvd-rvr-3b8dd1e2d238.json") # Service creation (with json keyfile) for accessing bucket
+# Global variables
+
+spark = SparkSession.builder \
+    .appName('main') \
+    .master('local[*]') \
+    .getOrCreate() 
+# Service creation (with json keyfile) to access google cloud bucket
+spark._jsc.hadoopConfiguration().set('fs.gs.auth.service.account.enable', 'true')
+spark._jsc.hadoopConfiguration().set('google.cloud.auth.service.account.json.keyfile', 'data/gvd-rvr-3b8dd1e2d238.json') 
 df_appearances, df_clubs, df_competitions, df_games, df_leagues, df_players, df_final = None, None, None, None, None, None, None
 app = Flask(__name__)
 CORS(app)
 
-#Function that loads data from "data" folder
+# Load data from 'data' folder
 def load_data_local():
     global df_appearances, df_clubs, df_competitions, df_games, df_leagues, df_players, df_final
     df_appearances = spark.read.csv('data/appearances.csv', inferSchema=True, header=True, sep=',')
@@ -22,25 +28,26 @@ def load_data_local():
     df_players = spark.read.csv('data/players.csv', inferSchema=True, header=True, sep=',')
     df_final = spark.read.csv('data/Final.csv', inferSchema=True, header=True, sep=';')
 
-#Function that loads data from Google bucket
+# Load data from google cloud bucket
 def load_data_bucket():
     global df_appearances, df_clubs, df_competitions, df_games, df_leagues, df_players, df_final
     bucket_name='data_football'
-    df_appearances = spark.read.csv(f"gs://{bucket_name}/appearances.csv", inferSchema=True, header=True, sep=',')
-    df_clubs = spark.read.csv(f"gs://{bucket_name}/clubs.csv", inferSchema=True, header=True, sep=',')
-    df_competitions = spark.read.csv(f"gs://{bucket_name}/competitions.csv", inferSchema=True, header=True, sep=',')
-    df_games = spark.read.csv(f"gs://{bucket_name}/games.csv", inferSchema=True, header=True, sep=',')
-    df_leagues = spark.read.csv(f"gs://{bucket_name}/leagues.csv", inferSchema=True, header=True, sep=',')
-    df_players = spark.read.csv(f"gs://{bucket_name}/players.csv", inferSchema=True, header=True, sep=',') 
-    df_final = spark.read.csv(f"gs://{bucket_name}/Final.csv", inferSchema=True, header=True, sep=';')
+    df_appearances = spark.read.csv(f'gs://{bucket_name}/appearances.csv', inferSchema=True, header=True, sep=',')
+    df_clubs = spark.read.csv(f'gs://{bucket_name}/clubs.csv', inferSchema=True, header=True, sep=',')
+    df_competitions = spark.read.csv(f'gs://{bucket_name}/competitions.csv', inferSchema=True, header=True, sep=',')
+    df_games = spark.read.csv(f'gs://{bucket_name}/games.csv', inferSchema=True, header=True, sep=',')
+    df_leagues = spark.read.csv(f'gs://{bucket_name}/leagues.csv', inferSchema=True, header=True, sep=',')
+    df_players = spark.read.csv(f'gs://{bucket_name}/players.csv', inferSchema=True, header=True, sep=',') 
+    df_final = spark.read.csv(f'gs://{bucket_name}/Final.csv', inferSchema=True, header=True, sep=';')
 
 ''' Show player name, total goals and assists and a media of minutes played '''
 @app.route('/api/jugador_resume', methods=['GET'])
 def jugador_resume():
     df_appearances.createOrReplaceTempView('sqlAppeareances')
     df_players.createOrReplaceTempView('sqlPlayers')
-    result = spark.sql(''' SELECT FIRST(sqlAppeareances.player_id) AS player_id, sqlPlayers.name, SUM(sqlAppeareances.goals), SUM(sqlAppeareances.assists), 
-    round(AVG(sqlAppeareances.minutes_played), 2) AS media_minutes_played, COUNT(sqlPlayers.player_id) AS games_played 
+    result = spark.sql(''' 
+    SELECT FIRST(sqlAppeareances.player_id) AS player_id, sqlPlayers.name, SUM(sqlAppeareances.goals), SUM(sqlAppeareances.assists), 
+           round(AVG(sqlAppeareances.minutes_played), 2) AS media_minutes_played, COUNT(sqlPlayers.player_id) AS games_played 
     FROM sqlPlayers 
     JOIN sqlAppeareances ON sqlPlayers.player_id = sqlAppeareances.player_id 
     GROUP BY sqlPlayers.name 
@@ -51,7 +58,8 @@ def jugador_resume():
 def jugador_position_foot_price():
     df_players.createOrReplaceTempView('sqlPlayers')
     df_final.createOrReplaceTempView('sqlFinal')
-    result = spark.sql(''' SELECT sqlPlayers.pretty_name, sqlPlayers.position, sqlPlayers.foot, sqlFinal.Player, sqlFinal.Market_value, sqlFinal.Club_x 
+    result = spark.sql(''' 
+    SELECT sqlPlayers.pretty_name, sqlPlayers.position, sqlPlayers.foot, sqlFinal.Player, sqlFinal.Market_value, sqlFinal.Club_x 
     FROM sqlPlayers 
     JOIN sqlFinal ON sqlPlayers.pretty_name = sqlFinal.Player 
     WHERE position = "Midfield" AND foot = "Right" ''').toJSON().collect()
@@ -61,13 +69,41 @@ def jugador_position_foot_price():
 def seleccion_inglesa():
     df_players.createOrReplaceTempView('sqlPlayers')
     df_final.createOrReplaceTempView('sqlFinal')
-    result = spark.sql(''' SELECT FIRST(sqlPlayers.pretty_name), FIRST(sqlPlayers.sub_position), FIRST(sqlFinal.Market_value), FIRST(sqlFinal.Nation) 
+    result = spark.sql(''' 
+    SELECT FIRST(sqlPlayers.pretty_name), FIRST(sqlPlayers.sub_position), FIRST(sqlFinal.Market_value), FIRST(sqlFinal.Nation) 
     FROM sqlPlayers 
     JOIN sqlFinal ON sqlPlayers.pretty_name = sqlFinal.Player 
     WHERE Nation = "ENG" 
     GROUP BY sqlPlayers.sub_position''').toJSON().collect()
     return json.dumps(result)
 
-if __name__ == "__main__":
+''' @victor00hs explica que hace esta funcion '''
+def jugador_position_foot_price():
+    df_players.createOrReplaceTempView('sqlPlayers')
+    df_final.createOrReplaceTempView('sqlFinal')
+    result = spark.sql(''' 
+        SELECT sqlPlayers.pretty_name, sqlPlayers.position, sqlPlayers.foot, sqlFinal.Player, sqlFinal.Market_value, sqlFinal.Club_x 
+        FROM sqlPlayers 
+        JOIN sqlFinal ON sqlPlayers.pretty_name = sqlFinal.Player 
+        WHERE position = 'Midfield' AND foot = 'Right'
+    ''').show()
+    return json.dumps(result)
+
+''' Displays the 15 most expensive players (biggest market value) along its player id, club id, country of citizenship
+    team, DOB, position, sub-position and market value '''
+@app.route('/api/most_expensive_players', methods=['GET'])
+def most_expensive_per_position():
+    df_players.createOrReplaceTempView('players')
+    df_clubs.createOrReplaceTempView('clubs')
+    query = spark.sql('''
+        SELECT player_id, players.pretty_name, current_club_id, clubs.pretty_name, country_of_citizenship position, sub_position, highest_market_value_in_gbp
+        FROM players
+        JOIN clubs ON clubs.club_id = players.current_club_id
+        ORDER BY highest_market_value_in_gbp DESC
+        LIMIT 15
+    ''').toJSON().collect()
+    return json.dumps(query)
+
+if __name__ == '__main__':
     load_data_bucket()
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,33 +1,7 @@
-import re
 import streamlit as st
 import pandas as pd
 import requests
 import json
-   
-def jugador_resume_table(data):
-    st.header('Información general jugadores')
-    res = json.loads(data)
-    for idx, i in enumerate(res):
-        res[idx] = json.loads(i)
-    df = pd.DataFrame(res).astype(str)
-    st.write(df)
-
-def jugador_position_foot_price_table(data):
-    st.header('Informacion jugador (foot) (position) (price)')
-    res = json.loads(data)
-    for idx, i in enumerate(res):
-        res[idx] = json.loads(i)
-    df = pd.DataFrame(res).astype(str)
-    st.write(df)
-
-def seleccion_inglesa_table(data):
-    st.header('11 Jugadores ingleses')
-    res = json.loads(data)
-    for idx, i in enumerate(res):
-        res[idx] = json.loads(i)
-    df = pd.DataFrame(res).astype(str)
-    st.write(df)
-
 
 def settings_st():
     # Page settings
@@ -45,18 +19,50 @@ def settings_st():
     )
     st.title('Valor de mercado de un jugador de futbol')
 
+# Explicar esta movie
+def load_json_data(data):
+    result = json.loads(data)
+    for idx, i in enumerate(result):
+        result[idx] = json.loads(i)
+    return result
+
+def jugador_position_foot_price_table(data):
+    st.header('Informacion jugador (foot) (position) (price)')
+    res = load_json_data(data)
+    df = pd.DataFrame(res).astype(str)
+    st.write(df)
+
+def seleccion_inglesa_table(data):
+    st.header('11 Jugadores ingleses')
+    res = load_json_data(data)
+    df = pd.DataFrame(res).astype(str)
+    st.write(df)
+
+def jugador_resume_table(data):
+    st.header('Información general jugadores')
+    res = load_json_data(data)
+    df = pd.DataFrame(res).astype(str)
+    st.write(df)
+
+def most_expensive_players(data):
+    res = load_json_data(data)
+    st.write(res)
+
 def mainpage_requests():
     r_jugador_resume = requests.get(url='http://127.0.0.1:5000/api/jugador_resume').content
     r_jugador_position_foot_price = requests.get(url='http://127.0.0.1:5000/api/jugador_position_foot_price').content
     r_seleccion_inglesa = requests.get(url='http://127.0.0.1:5000/api/seleccion_inglesa').content
+    r_most_expensive_players = requests.get(url='http://127.0.0.1:5000/api/most_expensive_players').content
 
     jugador_resume_table(r_jugador_resume)
     jugador_position_foot_price_table(r_jugador_position_foot_price)
     seleccion_inglesa_table(r_seleccion_inglesa)
+    most_expensive_players(r_most_expensive_players)
+
 
 if __name__ == '__main__':
     settings_st()
     try:
         mainpage_requests()
     except requests.exceptions.ConnectionError:
-        print('Connection refused')
+        print('Connection refused, try again later...')
