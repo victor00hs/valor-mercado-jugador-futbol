dd78a93051a0d793ff08e472d4bb359944c7ea05
victor00hs <victor00hs@gmail.com>
2022-01-22 19:46:04 +0100

    update header

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ def init_position(position):
 
 #GET request calls for "jugador_position_foot_price_table"
 def data_request_foot_position():
+    st.header('Selector de variables')
     r_foot = requests.get('http://127.0.0.1:5000/api/get_data_foot').content
     r_position = requests.get('http://127.0.0.1:5000/api/get_data_position').content
     init_foot(r_foot)
